045c38461d42a7eb7b9db035d5aaeacf59329dd6
Rafał Chłodnicki <rchl2k@gmail.com>
2020-03-08 11:29:39 +0100

    Run server from cache directory (#7)
    * Add sublime-command to open lsp-eslint settings
    
    * Run server from cache directory
    
     - Copy server to cache directory.
     - Install dependencies inside cache directory.
     - Re-copy and Re-install server on new version (based on server's
       package.json).
     - Don't include `.no-sublime-package` as there is no need to ship package
       unpacked anymore.
     - Remove unnecessary pacakge-lock.json and package.json from root dir.
     - Install only production npm dependencies
    
    * Refactor server resources class into separate utility file
    
    * Shorter varaiable names
    
    Co-authored-by: Predrag <idmpepe@gmail.com>

--- plugin.py
+++ plugin.py
@@ -1,92 +1,50 @@
 import os
 import shutil
 import sublime
-import subprocess
-import threading
 import webbrowser
 
 from LSP.plugin.core.handlers import LanguageHandler
 from LSP.plugin.core.protocol import Response
 from LSP.plugin.core.settings import ClientConfig
 from LSP.plugin.core.settings import read_client_config
+from .utils.resources import ServerNpmResource
 
+PACKAGE_NAME = 'LSP-eslint'
+SETTINGS_FILENAME = 'LSP-eslint.sublime-settings'
+SERVER_DIRECTORY = 'vscode-eslint'
+SERVER_BINARY_PATH = os.path.join(SERVER_DIRECTORY, 'out', 'eslintServer.js')
 
-package_path = os.path.dirname(__file__)
-server_path = os.path.join(package_path, 'vscode-eslint', 'out', 'eslintServer.js')
-vscode_eslint_path = os.path.join(package_path, 'vscode-eslint')
-node_modules_path = os.path.join(vscode_eslint_path, 'node_modules')
+server = ServerNpmResource(PACKAGE_NAME, SERVER_DIRECTORY, SERVER_BINARY_PATH)
 
 
 def plugin_loaded():
-    dependencies_insalled = os.path.isdir(node_modules_path)
-    print('LSP-eslint: Server {} installed.'.format('is' if dependencies_insalled else 'is not'))
-
-    if not dependencies_insalled:
-        # this will be called only when the plugin gets:
-        # - installed for the first time,
-        # - or when updated on package control
-        logAndShowMessage('LSP-eslint: Installing server.')
-
-        runCommand(
-            onCommandDone,
-            ["npm", "install", "--verbose", "--prefix", vscode_eslint_path, vscode_eslint_path]
-        )
-
-
-def onCommandDone():
-    logAndShowMessage('LSP-eslint: Server installed.')
-
-
-def runCommand(onExit, popenArgs):
-    """
-    Runs the given args in a subprocess.Popen, and then calls the function
-    onExit when the subprocess completes.
-    onExit is a callable object, and popenArgs is a list/tuple of args that
-    would give to subprocess.Popen.
-    """
-    def runInThread(onExit, popenArgs):
-        try:
-            if sublime.platform() == 'windows':
-                subprocess.check_call(popenArgs, shell=True)
-            else:
-                subprocess.check_call(popenArgs)
-            onExit()
-        except subprocess.CalledProcessError as error:
-            logAndShowMessage('LSP-eslint: Error while installing the server.', error)
-        return
-    thread = threading.Thread(target=runInThread, args=(onExit, popenArgs))
-    thread.start()
-    # returns immediately after the thread starts
-    return thread
+    server.setup()
 
 
 def is_node_installed():
     return shutil.which('node') is not None
 
 
-def logAndShowMessage(msg, additional_logs=None):
-    print(msg, '\n', additional_logs) if additional_logs else print(msg)
-    sublime.active_window().status_message(msg)
-
-
 class LspEslintPlugin(LanguageHandler):
     @property
     def name(self) -> str:
-        return 'lsp-eslint'
+        return PACKAGE_NAME
 
     @property
     def config(self) -> ClientConfig:
-        settings = sublime.load_settings("LSP-eslint.sublime-settings")
+        settings = sublime.load_settings(SETTINGS_FILENAME)
         client_configuration = settings.get('client')
         if client_configuration is None:
             client_configuration = {}
 
+        # Calling setup() also here as this might run before `plugin_loaded`.
+        # Will be a no-op if already ran.
+        # See https://github.com/sublimelsp/LSP/issues/899
+        server.setup()
+
         default_configuration = {
-            "command": [
-                'node',
-                server_path,
-                '--stdio'
-            ],
+            'enabled': True,
+            'command': ['node', server.binary_path, '--stdio'],
             "languages": [
                 {
                     "languageId": "eslint",
@@ -123,11 +81,11 @@ class LspEslintPlugin(LanguageHandler):
         }
 
         default_configuration.update(client_configuration)
-        return read_client_config('lsp-eslint', default_configuration)
+        return read_client_config(self.name, default_configuration)
 
     def on_start(self, window) -> bool:
         if not is_node_installed():
-            sublime.status_message('Please install Node.js for the ESLint Language Server to work.')
+            sublime.status_message("{}: Please install Node.js for the server to work.".format(PACKAGE_NAME))
             return False
         return True
 
