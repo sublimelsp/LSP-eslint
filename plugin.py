e219b5d3ae6c44c0e38d99fe2adf496f7c87372b
Raoul Wols <raoulwols@gmail.com>
2020-05-18 21:54:22 +0200

    Attempt with AbstractPlugin class

--- plugin.py
+++ plugin.py
@@ -2,13 +2,20 @@ import os
 import webbrowser
 
 from lsp_utils import NpmClientHandler
+from LSP.plugin import Response
+from LSP.plugin import register_plugin
+from LSP.plugin import unregister_plugin
+from LSP.plugin import LanguageConfig
+from LSP.plugin.core.typing import Optional, Dict, Any, List
 
 
 def plugin_loaded():
     LspEslintPlugin.setup()
+    register_plugin(LspEslintPlugin)
 
 
 def plugin_unloaded():
+    unregister_plugin(LspEslintPlugin)
     LspEslintPlugin.cleanup()
 
 
@@ -16,6 +23,37 @@ class LspEslintPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'vscode-eslint'
     server_binary_path = os.path.join(server_directory, 'out', 'eslintServer.js')
+
+    @classmethod
+    def languages(cls) -> List[LanguageConfig]:
+        return [LanguageConfig("javascript", "source.js"), LanguageConfig("vue", "text.html.vue")]
+
+    @classmethod
+    def initialization_options(cls) -> Optional[Dict[str, Any]]:
+        return {}
+
+    @classmethod
+    def default_settings(cls) -> Optional[Dict[str, Any]]:
+        return {
+            "validate": True,
+            "packageManager": "npm",
+            "autoFix": True,
+            "autoFixOnSave": True,
+            "options": {},
+            "run": "onType",
+            "nodePath": None,
+            "quiet": False,
+            "workspaceFolder": None,
+            "codeAction": {
+                "disableRuleComment": {
+                    "enable": True,
+                    "location": "separateLine"
+                },
+                "showDocumentation": {
+                    "enable": True
+                },
+            },
+        }
 
     def on_ready(self, api) -> None:
         api.on_notification('eslint/status', self.handle_status)
