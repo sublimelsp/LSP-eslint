9772c0f1619dc19feeddf35594383fca031f8af2
Rafał Chłodnicki <rchl2k@gmail.com>
2020-08-08 19:44:27 +0200

    Add support for resolving workspaceDirectories into workingDirectory
    Implements code for parsing user-supplied "workspaceDirectories" setting
    into "workingDirectory" that server receives. The relevant code from
    vscode-eslint extension is here:
    https://github.com/microsoft/vscode-eslint/blob/a296a01afc79089c49a4f5c9c2b75831a03494b0/client/src/extension.ts#L1183-L1255
    
    Configuring "workspaceDirectories" using patterns (rather than paths) is
    not supported as there is some crazy parsing code that is required for
    that and I didn't feel like converting that to python:
    https://github.com/microsoft/vscode-eslint/blob/a296a01afc79089c49a4f5c9c2b75831a03494b0/client/src/utils.ts#L77-L228
    
    Resolves #18

--- plugin.py
+++ plugin.py
@@ -1,6 +1,10 @@
 import os
+import posixpath
+import re
+import sublime
 import webbrowser
 
+from LSP.plugin.core.url import uri_to_filename
 from lsp_utils import NpmClientHandler
 
 
@@ -16,6 +20,14 @@ class LspEslintPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'language-server'
     server_binary_path = os.path.join(server_directory, 'out', 'eslintServer.js')
+
+    @classmethod
+    def observe_file_changes(cls):
+        return [
+            '**/.eslintr{c.js,c.yaml,c.yml,c,c.json}',
+            '**/.eslintignore',
+            '**/package.json',
+        ]
 
     def on_ready(self, api) -> None:
         api.on_notification('eslint/status', self.handle_status)
@@ -33,7 +45,77 @@ class LspEslintPlugin(NpmClientHandler):
         if session:
             scope_uri = params.get('scopeUri')
             if scope_uri:
+                workspace_folder = None
                 for folder in session.get_workspace_folders():
                     if folder.includes_uri(scope_uri):
-                        configuration['workspaceFolder'] = folder.to_lsp()
+                        workspace_folder = folder
                         break
+                if workspace_folder:
+                    configuration['workspaceFolder'] = workspace_folder.to_lsp()
+                self.resolve_working_directory(configuration, scope_uri, workspace_folder)
+
+    def resolve_working_directory(self, configuration, scope_uri, workspace_folder) -> None:
+        working_directories = configuration.get('workingDirectories', None)
+        if isinstance(working_directories, list):
+            working_directory = None
+            workspace_folder_path = workspace_folder.path if workspace_folder else None
+            for entry in working_directories:
+                directory = None
+                no_cwd = False
+                if isinstance(entry, str):
+                    directory = entry
+                elif self.is_directory_item(entry):
+                    directory = entry.directory
+                    if isinstance(entry.get('!cwd', None), bool):
+                        no_cwd = entry['!cwd']
+                elif self.is_pattern_item(entry):
+                    print('LSP-eslint: workingDirectories configuration that uses "pattern" is not supported')
+                    pass
+                elif self.is_mode_item(entry):
+                    working_directory = entry
+                    continue
+
+                directory_value = None
+                if directory:
+                    file_path = uri_to_filename(scope_uri)
+                    if directory:
+                        directory = self.to_os_path(directory)
+                        if not os.path.isabs(directory) and workspace_folder_path:
+                            # Trailing '' will add trailing slash if missing.
+                            directory = os.path.join(workspace_folder_path, directory, '')
+                        if file_path.startswith(directory):
+                            directory_value = directory
+
+                if directory_value:
+                    if not working_directory or self.is_mode_item(working_directory):
+                        working_directory = { 'directory': directory_value, '!cwd': no_cwd }
+                    else:
+                        if len(working_directory['directory']) < len(directory_value):
+                            working_directory['directory'] = directory_value
+                            working_directory['!cwd'] = no_cwd
+            configuration['workingDirectory'] = working_directory
+
+    def is_directory_item(self, item) -> bool:
+        if isinstance(item, dict):
+            directory = item.get('directory', None)
+            not_cwd = item.get('!cwd', None)
+            return isinstance(directory, str) and (isinstance(not_cwd, bool) or not_cwd == None)
+        return False
+
+    def is_pattern_item(self, item) -> bool:
+        if isinstance(item, dict):
+            pattern = item.get('pattern', None)
+            not_cwd = item.get('!cwd', None)
+            return isinstance(pattern, str) and (isinstance(not_cwd, bool) or not_cwd == None)
+        return False
+
+    def is_mode_item(self, item) -> bool:
+        if isinstance(item, dict):
+            mode = item.get('mode', None)
+            return isinstance(mode, str) and mode in ('auto', 'location')
+        return False
+
+    def to_os_path(self, path) -> str:
+        if sublime.platform == 'windows':
+            path = re.sub(r'^\/(\w)\/', r'\1:\\', path)
+        return os.path.normpath(path);
